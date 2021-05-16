4575d4370fc38d665888b0a12c0b0d8f9688786f
Dominic Zecchino <56906091+dzecchino@users.noreply.github.com>
2021-05-15 22:05:36 -0400

    Update websocket.go

--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -7,12 +7,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
+var upgrader = websocket.Upgrader{		//defining an upgrader from standard http to websocket endpoint
+	ReadBufferSize:  1024,			//necessary buffer sizes
 	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
+	CheckOrigin:     func(r *http.Request) bool { return true },	//validates request origin
 }
 
+//below function used to reply to client with an HTTP response error
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
