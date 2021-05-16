41594677aefe39e62f16d318d5180dcd11bef29f
Dominic Zecchino <56906091+dzecchino@users.noreply.github.com>
2021-05-15 21:58:40 -0400

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"fmt"		//fmt import formats I/O with functions for printing and reading
+	"net/http"	//thi import is for request-response handling for a web application
 
 	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
@@ -12,7 +12,7 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	fmt.Println("WebSocket Endpoint Hit")
 	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		fmt.Fprintf(w, "%+v\n", err)
+		fmt.Fprintf(w, "%+v\n", err)	//shows a resuling error in endpoint connection
 	}
 
 	client := &websocket.Client{
@@ -29,13 +29,13 @@ func setupRoutes() {
 	pool := websocket.NewPool()
 	go pool.Start()
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {		//mapping websocket endpoint to serveWs function
 		serveWs(pool, w, r)
 	})
 }
 
-func main() {
-	fmt.Println("Distributed Chat App v0.01")
-	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+func main() {						
+	fmt.Println("Distributed Chat App v0.01")	//simple print to show that it is a chat app
+	setupRoutes()					//setupRoutes function call
+	http.ListenAndServe(":8080", nil)		//listen on port 8080 while disregarding any secondary parameter (nil) for now
 }
