b8611265e2153823f59e0d21bf9d0246ed6ae5f4
Dominic Zecchino <56906091+dzecchino@users.noreply.github.com>
2021-05-17 05:45:13 -0400

    Update client.go

--- backend/pkg/websocket/client.go
+++ backend/pkg/websocket/client.go
@@ -7,15 +7,16 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type Client struct {
-	ID   string
-	Conn *websocket.Conn
-	Pool *Pool
+type Client struct {			//Client information structure
+	ID   string			//Identifying client IDs as type string
+	Conn *websocket.Conn		//This is a pointer to a websocket.Conn object
+					//Conn is a generic stream-oriented network connection
+	Pool *Pool			//pointer to the pool in which a client is a part of 
 }
 
-type Message struct {
+type Message struct {			//Message structure
 	Type int    `json:"type"`
-	Body string `json:"body"`
+	Body string `json:"body"`	//Defining the body of the message as a string
 }
 
 func (c *Client) Read() {
@@ -30,8 +31,8 @@ func (c *Client) Read() {
 			log.Println(err)
 			return
 		}
-		message := Message{Type: messageType, Body: string(p)}
-		c.Pool.Broadcast <- message
+		message := Message{Type: messageType, Body: string(p)}	//Setting the message type and body information
+		c.Pool.Broadcast <- message				//Looping through all clients in pool and sending through socket connection
 		fmt.Printf("Message Received: %+v\n", message)
 	}
 }
